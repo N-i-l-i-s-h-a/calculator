b28da8ce181e0b30618751f3e4a41b8db28dcc70
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-09-04 17:50:20 +0000

    display numbers on the screen

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-function operate(){
-    const op1 = document.querySelector('#input1').value;
-    console.log(`a = ${op1}`);
-    const op2 = document.querySelector('#input2').value;
-    console.log(`b = ${op2}`);
-    const operation = document.querySelector('input[name="operation"]:checked').value;
-    switch (operation) {
-        case '+':
-            const sum = Number(op1) + Number(op2);
-            console.log(`a + b = ${sum}`)
-            break;
-        case '-':
-            const diff = op1 - op2;
-            console.log(`a - b = ${diff}`)
-            break;
-        case 'x':
-            const prod = op1 *  op2;
-            console.log(`a x b = ${prod}`)
-            break;
-        case '/':
-            const quot = op1 /  op2;
-            console.log(`a / b = ${quot}`)
-            break;
-        default:
-            break;
-    }
-}
+const calc_dis = document.querySelector("#operation");
+/*numerical button -> eventListener that displays the number*/
+const button = document.querySelectorAll('#num');
+for(const btn of button){
+    btn.addEventListener('click', function(event){
+        calc_dis.innerText += (event.target).innerText;
+        calc_dis.classList.add('emphasized');
+    });
+} 
