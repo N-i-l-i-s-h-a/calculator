67679d7905e91cac16eca2d97a923f8fb1a17c7a
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-09-05 12:52:31 +0000

    stop numbers from overflowing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const calc_dis = document.querySelector("#operation");
 /*numerical button -> eventListener that displays the number*/
-const button = document.querySelectorAll('#num');
-for(const btn of button){
+const numBtn = document.querySelectorAll('#num');
+for(const btn of numBtn){
     btn.addEventListener('click', function(event){
         calc_dis.innerText += (event.target).innerText;
-        calc_dis.classList.add('emphasized');
     });
 }
+
