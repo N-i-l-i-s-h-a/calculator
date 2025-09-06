0205a731ed032e323805f3bb264b439db1e054c6
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-09-06 15:17:27 +0000

    add functionality to 4 out of 5 special buttons

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const calc_dis = document.querySelector("#operation");
 /*numerical button -> eventListener that displays the number*/
-const numBtn = document.querySelectorAll('#num');
-for(const btn of numBtn){
+const disBtn = document.querySelectorAll('.dis');
+for(const btn of disBtn){
     btn.addEventListener('click', function(event){
         calc_dis.innerText += (event.target).innerText;
     });
 }
 
+function clearAll(){
+    calc_dis.innerText = "";
+}
+
+function changeSign(){
+    calc_dis.innerText = 0 - Number(calc_dis.innerText);
+}
+
+function evalPercentage(){
+    calc_dis.innerText = Number(calc_dis.innerText) / 100;
+}
+
+function backspace(){
+    let num = calc_dis.innerText;
+    calc_dis.innerText = num.slice(0, -1);
+}
