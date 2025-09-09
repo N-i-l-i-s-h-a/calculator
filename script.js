b4fe43d2df3e35ef152e79681ef3010abd4d421d
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-09-09 12:27:35 +0000

    result is appended to the expression

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*dom elements inside display screen of calculator*/
-const res_dis = document.querySelector("#result");
-const calc_dis = document.querySelector("#operation");
+const res_dis = document.querySelector("#result"); //result is displayed
+const calc_dis = document.querySelector("#operation"); //expressions are made 
 
 /*to store the operands that comes before and after clicking the arithmetic ops buttons*/
 let operand1;
@@ -8,27 +8,30 @@ let operand2;
 let operation;
 
 /*numerical button -> eventListener that displays the number*/
-const disBtn = document.querySelectorAll('.dis');
-for(const btn of disBtn){
+const numBtn = document.querySelectorAll('.dis');
+for(const btn of numBtn){
     btn.addEventListener('click', function(event){
-        calc_dis.innerText += (event.target).innerText;
+        if(res_dis.innerText != ''){
+            calc_dis.style.fontSize = '40px';
+            calc_dis.innerText = res_dis.innerText + (event.target).innerText;
+        }
+        else{
+            calc_dis.innerText += (event.target).innerText;
+        }
     });
 }
 
 /*arithmetic button -> eventListener that initializes operand1*/
 const arithBtn = document.querySelectorAll('#arith');
 for(const btn of arithBtn){
-    operation = btn.innerText;
     btn.addEventListener('click', function(event){
-        if(res_dis.innerText === ''){
+        operation = btn.innerText;
+        if(res_dis.innerText === ""){
             operand1 = calc_dis.innerText;
             calc_dis.innerText += operation;
         }
         else{
-            operand1 = res_dis.innerText;
-            calc_dis.style.fontSize = '40px';
-            calc_dis.innerText = operand1;
-            res_dis.innerText = "";
+            resetDisWithRes();
             calc_dis.innerText += operation;
         }
     })
@@ -38,19 +41,43 @@ function clearAll(){
     calc_dis.innerText = "";
     res_dis.innerText = "";
     calc_dis.style.fontSize = '40px';
+    operation = '';
+    operand1 = '';
+    operand2 = '';
 }
 
 function changeSign(){
+    if(res_dis.innerText != ''){
+        resetDisWithRes();
+    }
+    if(operation != ''){
+        return;
+    }
     calc_dis.innerText = 0 - Number(calc_dis.innerText);
 }
 
 function evalPercentage(){
+    if(res_dis.innerText != ''){
+        resetDisWithRes();
+    }
     calc_dis.innerText = Number(calc_dis.innerText) / 100;
 }
 
 function backspace(){
-    let num = calc_dis.innerText;
-    calc_dis.innerText = num.slice(0, -1);
+    if(res_dis.innerText != ''){
+        resetDisWithRes();
+    }
+    calc_dis.innerText = calc_dis.innerText.slice(0, -1);
+        
+}
+
+function resetDisWithRes(){
+    operand2 = '';
+    operation = '';
+    operand1 = res_dis.innerText;
+    calc_dis.style.fontSize = '40px';
+    calc_dis.innerText = operand1;
+    res_dis.innerText = "";
 }
 
 function compute(){
